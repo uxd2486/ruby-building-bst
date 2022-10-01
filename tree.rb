d0342f823c6dafe0a886e9d9bdd7d30b1bc2dde9
Utkarsh Dayal <uxd2486@rit.edu>
2022-10-01 21:08:51 +0530

    Added the Tree class.

---
+++ tree.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# The Binary Search Tree
+class Tree
+  def initialize(array)
+    @root = build_tree(array)
+  end
+
+  def build_tree(array)
+    puts array
+  end
+end
