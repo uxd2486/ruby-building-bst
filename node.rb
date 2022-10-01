ae97abfa51ec30256ef47de53d08c46c88ff3772
Utkarsh Dayal <uxd2486@rit.edu>
2022-10-01 18:28:19 +0530

    Added the Node class

---
+++ node.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Represents one node in the binary search tree
+class Node
+  attr_accessor :value, :left, :right
+
+  include Comparable
+
+  def <=>(other)
+    @value <=> other.value
+  end
+
+  def initialize(value)
+    @value = value
+    @left = nil
+    @right = nil
+  end
+end
